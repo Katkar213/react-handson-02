16d8eb208b3d0287b48e86dce52e45c40ebec8b3
katkar213 <blackship213@gmail.com>
2023-09-01 23:48:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import UseEffect from "./component/UseEffect"
+import React from "react"
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      <UseEffect/>
+         
+
+    </div>
+  );
+}
+
+export default App;
